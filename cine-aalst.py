58d6ab9c997ac74ddc4503a3e0ee468836e5eb93
Kenny Van de Maele <kenny@kvandemaele.be>
2024-04-12 12:12:08 +0200

    black formatting

--- cine-aalst.py
+++ cine-aalst.py
@@ -20,6 +20,7 @@ warning_emojis = {
     "kw-nudity": "🍑",
 }
 
+
 def get_movies_and_schedules():
     url = "https://cine-aalst.be/nl/movies/today"
     response = requests.get(url)
@@ -30,102 +31,130 @@ def get_movies_and_schedules():
     if match:
         json_data = match.group(1)
         data = json.loads(json_data)
-        movies = data.get('movies', [])
-        schedules = data.get('schedules', [])
-        cinemas = data.get('cinemas', [])
-        screens = data.get('screens', [])
+        movies = data.get("movies", [])
+        schedules = data.get("schedules", [])
+        cinemas = data.get("cinemas", [])
+        screens = data.get("screens", [])
         return movies, schedules, cinemas, screens
     else:
         print("JSON data not found.")
         return [], [], [], []
 
+
 def filter_schedules_by_date(schedules, target_date):
     filtered_schedules = []
     for schedule in schedules:
-        start_time = datetime.fromisoformat(schedule['start'].split('+')[0])
+        start_time = datetime.fromisoformat(schedule["start"].split("+")[0])
         if start_time.date() == target_date:
             filtered_schedules.append(schedule)
     return filtered_schedules
 
+
 def aggregate_schedules_by_movie(schedules):
     aggregated_schedules = {}
     for schedule in schedules:
-        movie_id = schedule['movie_id']
+        movie_id = schedule["movie_id"]
         if movie_id not in aggregated_schedules:
             aggregated_schedules[movie_id] = []
         aggregated_schedules[movie_id].append(schedule)
     return aggregated_schedules
 
+
 def get_cinema_details(cinemas, cinema_id):
     for cinema in cinemas:
-        if cinema['id'] == cinema_id:
+        if cinema["id"] == cinema_id:
             return cinema
     return None
 
+
 def get_screen_details(screens, screen_id):
     for screen in screens:
-        if screen['id'] == screen_id:
+        if screen["id"] == screen_id:
             return screen
     return None
 
+
 def print_movie(movie, schedules, cinemas, screens):
-    movie_name = movie['title']
-    options = movie.get('options', [])
-    warning_symbols = [warning_emojis.get(option, '') for option in options]
+    movie_name = movie["title"]
+    options = movie.get("options", [])
+    warning_symbols = [warning_emojis.get(option, "") for option in options]
     print(f"\033[94m{movie_name}\033[0m:")
     print(f"  - \033[1mWarnings:\033[0m {', '.join(warning_symbols)}")
-    
+
     # Check if description exists before removing HTML tags
-    description = movie.get('description', '')
+    description = movie.get("description", "")
     if description:
-        description = re.sub('<[^<]+?>', '', description)
+        description = re.sub("<[^<]+?>", "", description)
         print(f"  - \033[1mDescription:\033[0m {description}")
     print(f"  - \033[1mDirector:\033[0m {movie['director']}")
-    if movie['actors'] != None:
+    if movie["actors"] != None:
         print(f"  - \033[1mActors:\033[0m {movie['actors']}")
-    print(f"  - \033[1mPoster:\033[0m \x1B]8;;https://cine-aalst.be{movie['poster']}\x1B\\Link\x1B]8;;\x1B\\")
-    print(f"  - \033[1mTrailer:\033[0m \x1B]8;;https:{movie['trailer']}\x1B\\Link\x1B]8;;\x1B\\")
+    print(
+        f"  - \033[1mPoster:\033[0m \x1B]8;;https://cine-aalst.be{movie['poster']}\x1B\\Link\x1B]8;;\x1B\\"
+    )
+    print(
+        f"  - \033[1mTrailer:\033[0m \x1B]8;;https:{movie['trailer']}\x1B\\Link\x1B]8;;\x1B\\"
+    )
     print(f"  - \033[1mRuntime:\033[0m {movie['runtime']} minutes")
-    if movie['nation'] != None:
+    if movie["nation"] != None:
         print(f"  - \033[1mNation:\033[0m {movie['nation']}")
-    release_date = datetime.fromisoformat(movie['release'].split('+')[0])
+    release_date = datetime.fromisoformat(movie["release"].split("+")[0])
     print(f"  - \033[1mRelease Date:\033[0m {release_date.strftime('%Y-%m-%d')}")
     print("  - \033[1mSchedules:\033[0m")
     for schedule in schedules:
-        start_time = datetime.fromisoformat(schedule['start'].split('+')[0])
-        cinema_details = get_cinema_details(cinemas, schedule['cinema_id'])
-        screen_details = get_screen_details(screens, schedule['screen_id'])
-        accessibility = "♿️" if screen_details and screen_details['accessible_disabled'] else ""
-        mask_emoji = "😷" if screen_details and screen_details['config']['social_distancing_enabled'] else ""
+        start_time = datetime.fromisoformat(schedule["start"].split("+")[0])
+        cinema_details = get_cinema_details(cinemas, schedule["cinema_id"])
+        screen_details = get_screen_details(screens, schedule["screen_id"])
+        accessibility = (
+            "♿️" if screen_details and screen_details["accessible_disabled"] else ""
+        )
+        mask_emoji = (
+            "😷"
+            if screen_details and screen_details["config"]["social_distancing_enabled"]
+            else ""
+        )
         ticket_link = f"https://cine-aalst.be/nl/buy/tickets/{schedule['id']}"
-        cinema_name = cinema_details['name']
+        cinema_name = cinema_details["name"]
         cinema_address = f"{cinema_details['address']['address1']}, {cinema_details['address']['city']}"
-        zaal_name = screen_details['name']
-        print(f"    - {start_time.strftime('%A, %d %B %H:%M')}{accessibility}{mask_emoji} - {zaal_name} ({cinema_name}, {cinema_address}) - \x1B]8;;{ticket_link}\x1B\\Tickets\x1B]8;;\x1B\\")
+        zaal_name = screen_details["name"]
+        print(
+            f"    - {start_time.strftime('%A, %d %B %H:%M')}{accessibility}{mask_emoji} - {zaal_name} ({cinema_name}, {cinema_address}) - \x1B]8;;{ticket_link}\x1B\\Tickets\x1B]8;;\x1B\\"
+        )
 
     print("---")
+
 
 def print_movies_by_date(movies, schedules, cinemas, screens, target_date):
     if target_date == None:
         print("\033[1mMovies and Schedules for all dates:\033[0m")
     else:
-        print(f"\033[1mMovies and Schedules for {target_date.strftime('%Y-%m-%d')}:\033[0m")
+        print(
+            f"\033[1mMovies and Schedules for {target_date.strftime('%Y-%m-%d')}:\033[0m"
+        )
     aggregated_schedules = aggregate_schedules_by_movie(schedules)
     for movie_id, movie_schedules in aggregated_schedules.items():
         for movie in movies:
-            if movie['id'] == movie_id:
+            if movie["id"] == movie_id:
                 print_movie(movie, movie_schedules, cinemas, screens)
                 break
 
+
 def print_movies_by_title(search_query, movies, schedules, cinemas, screens):
-    matching_movies = [movie for movie in movies if search_query.lower() in movie['title'].lower()]
+    matching_movies = [
+        movie for movie in movies if search_query.lower() in movie["title"].lower()
+    ]
     if matching_movies:
         print(f"\033[1mMovies matching '{search_query}':\033[0m")
         for movie in matching_movies:
-            movie_schedules = [schedule for schedule in schedules if schedule['movie_id'] == movie['id']]
+            movie_schedules = [
+                schedule
+                for schedule in schedules
+                if schedule["movie_id"] == movie["id"]
+            ]
             print_movie(movie, movie_schedules, cinemas, screens)
     else:
         print(f"No movies found matching '{search_query}'.")
+
 
 def parse_date(date_str):
     if date_str == "today":
@@ -135,13 +164,25 @@ def parse_date(date_str):
     try:
         return datetime.strptime(date_str, "%Y-%m-%d").date()
     except ValueError:
-        raise argparse.ArgumentTypeError("Invalid date format. Please use YYYY-MM-DD, 'today' or 'tomorrow'.")
+        raise argparse.ArgumentTypeError(
+            "Invalid date format. Please use YYYY-MM-DD, 'today' or 'tomorrow'."
+        )
+
 
 def main():
     parser = argparse.ArgumentParser(description="Get movie schedules for Cine Aalst.")
-    parser.add_argument("-d", "--date", type=parse_date,
-                        help="Date for which to get movie schedules. Can be 'today', 'tomorrow', or YYYY-MM-DD format. Can be combined with -m.")
-    parser.add_argument("-m", "--movie", type=str, help="Search for a movie by title. Can be combined with -d.")
+    parser.add_argument(
+        "-d",
+        "--date",
+        type=parse_date,
+        help="Date for which to get movie schedules. Can be 'today', 'tomorrow', or YYYY-MM-DD format. Can be combined with -m.",
+    )
+    parser.add_argument(
+        "-m",
+        "--movie",
+        type=str,
+        help="Search for a movie by title. Can be combined with -d.",
+    )
     args = parser.parse_args()
 
     if args.movie:
@@ -163,6 +204,6 @@ def main():
             filtered_schedules = schedules  # All schedules
         print_movies_by_date(movies, filtered_schedules, cinemas, screens, args.date)
 
+
 if __name__ == "__main__":
     main()
-
